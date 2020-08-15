ece1a2450f9dcd4beb8774a32a9293738cfbc5cc
Aleli <jann.aleli@gmail.com>
2020-08-15 20:40:32 +0800

    Added Image component

---
+++ src/components/UI/Images/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Image.css';
+
+const image = (props) => {
+    return <React.Fragment>
+
+    </React.Fragment>
+}
+
+
+export default image;
---
+++ src/containers/MainContainer/MainContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import classes from './MainContainer.css';
+
+class MainContainer extends Component {
+    render () {
+
+        return <React.Fragment>
+            
+        </React.Fragment>
+
+    }
+}
+
+export default MainContainer;
